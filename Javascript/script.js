18b17b0c32a91ee3c9750a57727231cc3b129992
aryanvashi <aryanvashishtha30@gmail.com>
2026-02-26 12:20:25 +0530

    data types,variables types, opeators

---
+++ Javascript/script.js
@@ -0,0 +1,58 @@
+// // // // console.log("Hello World!");
+// // // // introduction to variables:var;
+// // // {var a=20;
+// // // a="aryan";
+// // // console.log(a);}
+
+// // // console.log(a);
+
+// // // // introduction to variables:let;
+// // {    let name;
+// //     name="aryan";
+// //     name="aryan vashishtha";
+// //     console.log(name);
+// // }
+// // let name="anju";
+// // console.log(name);
+
+// // // introduction to variables:const;
+// {
+//     const name="aryan";
+//     console.log(name);
+// }
+// const name="aryan";
+// console.log(name);
+let a=10;
+let b=5;
+console.log(a+b);//15
+console.log(a-b);//5
+console.log(a*b);//50
+console.log(a/b);//2
+console.log(a++);//10
+console.log(++a);//12
+console.log(a--);//12
+console.log(--a);//10
+console.log(a);//10
+
+//operators(•	Arithmetic Operators: +, -, *, /, % (modulus), ++ (increment), -- (decrement).)
+/*let a=10;
+let b=5;
+console.log(a+b);//15
+console.log(a-b);//5
+console.log(a/b);//2
+console.log(a*b);//50
+console.log(a++);//10
+console.log(++a);//12
+console.log(--a);//11
+console.log(a--);//11
+console.log(a);//10*/
+//•	Assignment Operators: =, +=, -=, *=, /=, etc.
+let a=10;
+a+=5;//a=a+5
+console.log(a);
+a-=3;//a=a-3
+console.log(a);
+a*=2;//a=a*2
+console.log(a);
+a/=4;//a=a/4
+console.log(a);
