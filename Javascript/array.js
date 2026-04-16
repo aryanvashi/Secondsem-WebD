78ded5394a48d7471aa2cb9022b2ace9606527c1
aryanvashi <aryanvashishtha30@gmail.com>
2026-04-16 12:17:25 +0530

    different functions with no or more parameters

--- Javascript/array.js
+++ Javascript/array.js
@@ -24,6 +24,47 @@
 // console.log(mixedArray[4]);
 // console.log(mixedArray[5]);
 
-let newarray = [10, 20, 30, 40, 50];
-let squardarray = newarray.map(num => num * num);
-console.log(squardarray); 
+// let newarray = [10, 20, 30, 40, 50];
+// let squardarray = newarray.map(num => num * num);
+// console.log(squardarray); 
+
+// function greet(){
+//     console.log("Hello, World!");
+// }
+// greet();
+
+// normal function
+function multiply(a, b){
+    return a * b;
+}
+console.log(multiply(5,10));
+// arrow function
+let add =(a,b) => a+b;
+console.log(add(2,4));
+
+let fruits = ["Apple", "Banana", "Cherry"];
+fruits.forEach(fruit => console.log(fruit));
+
+//function expression
+let divide = function(a,b) {
+    return a / b;
+}
+console.log(divide(10,2));
+
+// arrow function with no parameters
+let sayHello = () => console.log("Hello!");
+sayHello();
+
+//arrow function with one parameter
+let square = x => x * x;
+console.log(square(5));
+
+//arrow function  
+const add= (a,b) => a+b;
+console.log(add(3,7));
+
+//arrow function with no parameters
+const greetUser = (name) => `Hello, ${name}!`;
+console.log(greetUser("Alice"));
+greetUser("Bob");
+ 
